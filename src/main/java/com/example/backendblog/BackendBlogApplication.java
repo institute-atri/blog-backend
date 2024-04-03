565b41899701566d02322361d82196d9bfc2cb57
Rafael Silva <ainbot6@gmail.com>
2024-04-03 17:23:01 -0300

    start project

---
+++ src/main/java/com/example/backendblog/BackendBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendblog/BackendBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
