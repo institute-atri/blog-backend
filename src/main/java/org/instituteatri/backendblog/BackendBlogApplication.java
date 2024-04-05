922de73ebfc85d4e93db0e9b0a42d793ed8332b6
Rafael Silva <ainbot6@gmail.com>
2024-04-05 02:59:14 -0300

    folder renamed to org.instituteatri.backendblog

--- src/main/java/org/instituteatri/backendblog/BackendBlogApplication.java
+++ src/main/java/org/instituteatri/backendblog/BackendBlogApplication.java
@@ -1,4 +1,4 @@
-package com.example.backendblog;
+package org.instituteatri.backendblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/instituteatri/backendblog/BackendBlogApplicationTests.java
+++ src/test/java/org/instituteatri/backendblog/BackendBlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.backendblog;
+package org.instituteatri.backendblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
