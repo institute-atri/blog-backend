c8b8a801ad94aa5a0b7775e13bbb17d3d153a4ce
Rafael Silva <ainbot6@gmail.com>
2024-04-11 00:36:48 -0300

    fix: refactoring to add constructors and other properties

--- src/main/java/org/instituteatri/backendblog/domain/entities/User.java
+++ src/main/java/org/instituteatri/backendblog/domain/entities/User.java
@@ -5,21 +5,86 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User  implements UserDetails {
     @Id
     private String id;
     private String name;
+    private String lastName;
+    private String phoneNumber;
+    private String bio;
     private String email;
     private String password;
+    private boolean isActive;
+
     private UserRole role;
     private List<Post> posts = new ArrayList<>();
+
+
+    public User(String name, String lastName,String phoneNumber, String bio, String email, String password, boolean isActive, UserRole role) {
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.bio = bio;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+        this.role = role;
+    }
+
+    public User(String name, String lastName,String phoneNumber, String email, String password, boolean isActive) {
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
