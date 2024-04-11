1acf0fcddb7cbe5661ef0b321fcf73771bc49e6a
Rafael Silva <ainbot6@gmail.com>
2024-04-11 18:07:55 -0300

    feat: creating a security filter without business rules #7

---
+++ src/main/java/org/instituteatri/backendblog/infrastructure/security/SecurityFilter.java
@@ -0,0 +1,29 @@
+package org.instituteatri.backendblog.infrastructure.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.instituteatri.backendblog.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    final TokenService tokenService;
+    final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
