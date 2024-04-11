4d9c81b06ef02bbf126e304bf30b0ce055d627c0
Rafael Silva <ainbot6@gmail.com>
2024-04-11 00:33:37 -0300

    feat: add a new class tag

---
+++ src/main/java/org/instituteatri/backendblog/domain/entities/Tag.java
@@ -0,0 +1,36 @@
+package org.instituteatri.backendblog.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class Tag implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String name;
+    private String slug;
+
+    private List<Post> posts = new ArrayList<>();
+
+    public Tag(String name, String slug){
+        this.name = name;
+        this.slug = slug;
+    }
+}
