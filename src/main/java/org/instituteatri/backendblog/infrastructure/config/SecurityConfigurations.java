97ca929aef74aa08bdb0451456bb485fc683c992
Rafael Silva <ainbot6@gmail.com>
2024-04-11 18:14:41 -0300

    feat: create a security configurations class without business rules #7

---
+++ src/main/java/org/instituteatri/backendblog/infrastructure/config/SecurityConfigurations.java
@@ -0,0 +1,19 @@
+package org.instituteatri.backendblog.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfigurations {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
