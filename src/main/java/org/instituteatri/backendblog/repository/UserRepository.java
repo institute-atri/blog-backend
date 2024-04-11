82b0f347812730ba38396860f0f2cb88da393fc6
Rafael Silva <ainbot6@gmail.com>
2024-04-11 18:04:45 -0300

    fix: small change to import from userdetails.User to entities.User #7

--- src/main/java/org/instituteatri/backendblog/repository/UserRepository.java
+++ src/main/java/org/instituteatri/backendblog/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package org.instituteatri.backendblog.repository;
 
+import org.instituteatri.backendblog.domain.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
