4d29d582667afd25f5030ef467b161741f28cc94
Rafael Silva <ainbot6@gmail.com>
2024-04-18 04:59:31 -0300

    fix: refactored (#1)

--- src/main/java/org/instituteatri/backendblog/infrastructure/security/SecurityFilter.java
+++ src/main/java/org/instituteatri/backendblog/infrastructure/security/SecurityFilter.java
@@ -26,19 +26,26 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain) throws ServletException, IOException {
-    var token = this.recoverToken(request);
-    if (token !=null){
-        var email = tokenService.validateToken(token);
-        UserDetails userDetails = userRepository.findByEmail(email);
-        if (userDetails != null){
-        var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
 
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var email = tokenService.validateToken(token);
+            UserDetails userDetails = userRepository.findByEmail(email);
+
+            if (userDetails != null) {
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
-    }
         filterChain.doFilter(request, response);
     }
+
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
         if (authHeader == null) return null;
