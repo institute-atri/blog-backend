b247fabc475f230d4fa84977c0af756344043a02
Rafael Silva <ainbot6@gmail.com>
2024-04-19 00:37:06 -0300

    feat: add cors (#22)

---
+++ src/main/java/org/instituteatri/backendblog/infrastructure/cors/CorsConfig.java
@@ -0,0 +1,18 @@
+package org.instituteatri.backendblog.infrastructure.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
