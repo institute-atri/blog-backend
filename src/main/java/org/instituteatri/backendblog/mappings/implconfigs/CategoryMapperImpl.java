fec09f90d41cd73fada951bd801498894bd410ae
Rafael Silva <ainbot6@gmail.com>
2024-04-20 16:00:53 -0300

    feat: add a new mapper implements CategoryMapperImpl (#21)

---
+++ src/main/java/org/instituteatri/backendblog/mappings/implconfigs/CategoryMapperImpl.java
@@ -0,0 +1,40 @@
+package org.instituteatri.backendblog.mappings.implconfigs;
+
+import org.instituteatri.backendblog.domain.entities.Category;
+import org.instituteatri.backendblog.dtos.CategoryDTO;
+import org.instituteatri.backendblog.dtos.PostDTO;
+import org.instituteatri.backendblog.mappings.CategoryMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+@Mapper(componentModel = "spring")
+@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public CategoryDTO toCategoryDto(Category category) {
+        return new CategoryDTO(
+                category.getId(),
+                category.getName(),
+                category.getSlug(),
+                category.getPosts().stream().map(post -> new PostDTO(
+                        post.getId(),
+                        post.getTitle(),
+                        post.getSummary(),
+                        post.getBody(),
+                        post.getSlug(),
+                        post.getCreatedAt(),
+                        post.getUpdatedAt(),
+                        post.getAuthorDTO(),
+                        post.getCategories(),
+                        post.getTags(),
+                        post.getComments()
+                )).collect(Collectors.toList())
+        );
+    }
+}
