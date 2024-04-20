2f10770900bc2bc930f998f1fd8c7c3cc406a73c
Rafael Silva <ainbot6@gmail.com>
2024-04-20 15:57:34 -0300

    refactor: refactor Category and Tag entities for better structure and funcionality (#21)

--- src/main/java/org/instituteatri/backendblog/domain/entities/Tag.java
+++ src/main/java/org/instituteatri/backendblog/domain/entities/Tag.java
@@ -1,12 +1,9 @@
 package org.instituteatri.backendblog.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -17,28 +14,19 @@ import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Document
-public class Tag implements Serializable {
+public class Tag extends BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     private String id;
-    private String name;
-    private String slug;
 
     @DBRef(lazy = true)
     @JsonIgnore
     private List<Post> posts = new ArrayList<>();
 
-    @JsonIgnore
-    @Transient
-    private int postCount;
-
-    public Tag(String name, String slug){
-        this.name = name;
-        this.slug = slug;
+    public Tag(String name, String slug) {
+        super(name, slug);
     }
 }
