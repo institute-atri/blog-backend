d58bc8b44b88b35bdeffcecbd407c2ca27d94408
Rafael Silva <ainbot6@gmail.com>
2024-04-20 00:54:25 -0300

    feat: inclusion of a counter to increase the number of posts in a tag (#21)

--- src/main/java/org/instituteatri/backendblog/domain/entities/Tag.java
+++ src/main/java/org/instituteatri/backendblog/domain/entities/Tag.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -31,6 +32,10 @@ public class Tag implements Serializable {
     @DBRef(lazy = true)
     @JsonIgnore
     private List<Post> posts = new ArrayList<>();
+
+    @JsonIgnore
+    @Transient
+    private int postCount;
 
     public Tag(String name, String slug){
         this.name = name;
