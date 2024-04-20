e9f41a5eb50b1564f62c137e972bf32076ff1373
Rafael Silva <ainbot6@gmail.com>
2024-04-20 17:19:56 -0300

    feat: add a new basic test (#21)

--- src/test/java/org/instituteatri/backendblog/BackendBlogApplicationTests.java
+++ src/test/java/org/instituteatri/backendblog/BackendBlogApplicationTests.java
@@ -3,11 +3,15 @@ package org.instituteatri.backendblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class BackendBlogApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		String message = "Context loaded successfully!";
 
+		assertNotNull(message, "Message should not be null");
+	}
 }
