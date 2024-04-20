e5dee81c625ce58e2756ecfb93b57548d806c566
Rafael Silva <ainbot6@gmail.com>
2024-04-20 01:20:21 -0300

    refactor: refactor the AccountService to improve the organization and readability of the code (#21)

--- src/main/java/org/instituteatri/backendblog/service/AccountService.java
+++ src/main/java/org/instituteatri/backendblog/service/AccountService.java
@@ -4,12 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.instituteatri.backendblog.domain.entities.User;
 import org.instituteatri.backendblog.domain.entities.UserRole;
 import org.instituteatri.backendblog.dtos.AuthenticationDTO;
+import org.instituteatri.backendblog.dtos.RegisterDTO;
 import org.instituteatri.backendblog.dtos.ResponseDTO;
-import org.instituteatri.backendblog.dtos.UserDTO;
 import org.instituteatri.backendblog.infrastructure.exceptions.EmailAlreadyExistsException;
+import org.instituteatri.backendblog.infrastructure.exceptions.PasswordsNotMatchException;
 import org.instituteatri.backendblog.infrastructure.security.TokenService;
 import org.instituteatri.backendblog.repository.UserRepository;
-import org.instituteatri.backendblog.service.helpers.auth.HelperAuthComponentLogin;
+import org.instituteatri.backendblog.service.components.authComponents.AccountLoginComponent;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -29,7 +30,8 @@ public class AccountService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final TokenService tokenService;
-    private final HelperAuthComponentLogin helperAuthComponent;
+    private final AccountLoginComponent accountLoginComponent;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username);
@@ -37,28 +39,32 @@ public class AccountService implements UserDetailsService {
 
     public ResponseEntity<ResponseDTO> processLogin(AuthenticationDTO authDto, AuthenticationManager authManager) {
         try {
-            var authResult = helperAuthComponent.authenticateUser(authDto, authManager);
+            var authResult = accountLoginComponent.authenticateUserComponent(authDto, authManager);
             var user = (User) authResult.getPrincipal();
 
-            helperAuthComponent.handleSuccessfulLogin(user);
+            accountLoginComponent.handleSuccessfulLoginComponent(user);
 
             var token = tokenService.generateToken(user);
             return ResponseEntity.ok(new ResponseDTO(token, user.getEmail()));
 
         } catch (LockedException e) {
-            return helperAuthComponent.handleLockedAccount();
+            return accountLoginComponent.handleLockedAccountComponent();
 
         } catch (BadCredentialsException e) {
-            return helperAuthComponent.handleBadCredentials(authDto.email());
+            return accountLoginComponent.handleBadCredentialsComponent(authDto.email());
         }
     }
 
 
-    public ResponseEntity<ResponseDTO> processRegister(UserDTO userDto) {
-        if (emailExists(userDto.email())) {
+    public ResponseEntity<ResponseDTO> processRegister(RegisterDTO registerDTO) {
+        if (isEmailExists(registerDTO.email())) {
             throw new EmailAlreadyExistsException();
         }
-        User newUser = createUser(userDto);
+
+        if (!registerDTO.password().equals(registerDTO.confirmPassword())) {
+            throw new PasswordsNotMatchException();
+        }
+        User newUser = createUser(registerDTO);
         User savedUser = userRepository.insert(newUser);
         URI uri = buildUserUri(savedUser);
 
@@ -67,22 +73,21 @@ public class AccountService implements UserDetailsService {
         return ResponseEntity.created(uri).body(new ResponseDTO(token, savedUser.getEmail()));
     }
 
-    private boolean emailExists(String email) {
+    private boolean isEmailExists(String email) {
         return loadUserByUsername(email) != null;
     }
 
-    private User createUser(UserDTO userDto) {
-        String encryptedPassword = new BCryptPasswordEncoder().encode(userDto.password());
-        UserRole role = UserRole.USER;
+    private User createUser(RegisterDTO registerDTO) {
+        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
         return new User(
-                userDto.name(),
-                userDto.lastName(),
-                userDto.phoneNumber(),
-                userDto.bio(),
-                userDto.email(),
+                registerDTO.name(),
+                registerDTO.lastName(),
+                registerDTO.phoneNumber(),
+                registerDTO.bio(),
+                registerDTO.email(),
                 encryptedPassword,
                 true,
-                role
+                UserRole.USER
         );
     }
 
