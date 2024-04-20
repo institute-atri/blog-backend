3fe4ba28bd1ea05ba0e5f6b1318417ff0f4f53af
Rafael Silva <ainbot6@gmail.com>
2024-04-20 01:17:12 -0300

    refactor: refactor the PostService to improve the organization and readability of the code (#21)

--- src/main/java/org/instituteatri/backendblog/service/PostService.java
+++ src/main/java/org/instituteatri/backendblog/service/PostService.java
@@ -1,77 +1,102 @@
 package org.instituteatri.backendblog.service;
 
 import lombok.RequiredArgsConstructor;
-import org.instituteatri.backendblog.domain.entities.Category;
 import org.instituteatri.backendblog.domain.entities.Post;
 import org.instituteatri.backendblog.domain.entities.User;
 import org.instituteatri.backendblog.dtos.PostDTO;
+import org.instituteatri.backendblog.infrastructure.exceptions.NotAuthenticatedException;
 import org.instituteatri.backendblog.infrastructure.exceptions.PostNotFoundException;
+import org.instituteatri.backendblog.mappings.PostMapper;
 import org.instituteatri.backendblog.repository.PostRepository;
-import org.instituteatri.backendblog.service.helpers.HelperValidateUser;
-import org.instituteatri.backendblog.service.helpers.helpPost.HelperComponentCreatePost;
-import org.instituteatri.backendblog.service.helpers.helpPost.HelperComponentPostDelete;
-import org.instituteatri.backendblog.service.helpers.helpPost.HelperComponentPostUpdate;
+import org.instituteatri.backendblog.service.components.postComponents.PostCreateComponent;
+import org.instituteatri.backendblog.service.components.postComponents.PostDeleteComponent;
+import org.instituteatri.backendblog.service.components.postComponents.PostUpdateComponent;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
-    private final HelperValidateUser helperValidateUser;
-    private final HelperComponentCreatePost helperComponentCreatePost;
-    private final HelperComponentPostUpdate helperComponentPostUpdate;
-    private final HelperComponentPostDelete helperComponentPostDelete;
+    private final PostMapper postMapper;
+    private final PostUpdateComponent postUpdateComponent;
+    private final PostCreateComponent postCreateComponent;
+    private final PostDeleteComponent postDeleteComponent;
 
-    public List<Post> findAllPosts() {
-        return postRepository.findAll();
+
+    public ResponseEntity<List<PostDTO>> processFindAllPosts() {
+        List<Post> posts = postRepository.findAll();
+
+        return ResponseEntity.ok(posts.stream().map(postMapper::toPostDto).collect(Collectors.toList()));
     }
 
-    public Post findById(String id) {
+    public PostDTO processFindById(String id) {
         Optional<Post> post = postRepository.findById(id);
-        return post.orElseThrow(() -> new PostNotFoundException(id));
+
+        return post.map(postMapper::toPostDto).orElseThrow(() -> new PostNotFoundException(id));
     }
 
-    public ResponseEntity<Post> processCreatePost(PostDTO postDTO, Authentication authentication) {
-        User currentUser = helperValidateUser.getCurrentUser(authentication);
-        helperValidateUser.validateCurrentUser(currentUser);
+    public ResponseEntity<PostDTO> processCreatePost(PostDTO postDTO, Authentication authentication) {
+        User currentUser = getCurrentUser(authentication);
+        validateCurrentUser(currentUser);
 
-        Post createdPost = helperComponentCreatePost.helperCreateNewPost(postDTO, currentUser);
+        PostDTO createdPost = postCreateComponent.createNewPostDTOComponent(postDTO, currentUser);
 
-        URI uri = helperComponentCreatePost.helperBuildUserUri(createdPost);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdPost.id())
+                .toUri();
 
         return ResponseEntity.created(uri).body(createdPost);
     }
 
-
     public ResponseEntity<Void> processUpdatePost(String id, PostDTO updatedPostDto, User currentUser) {
-        helperValidateUser.validateCurrentUser(currentUser);
+        validateCurrentUser(currentUser);
 
-        Post existingPost = findById(id);
+        Post existingPost = postUpdateComponent.findPostByIdComponent(id);
 
-        helperComponentPostUpdate.helperValidateAuthorship(existingPost, currentUser);
+        postUpdateComponent.authorizePostUpdateComponent(existingPost, currentUser);
 
-        helperComponentPostUpdate.helperUpdate(id, updatedPostDto);
+        postUpdateComponent.updatePostPropertiesComponent(existingPost, updatedPostDto);
+
+        postRepository.save(existingPost);
 
         return ResponseEntity.noContent().build();
     }
 
-    public void deletePost(String id, Authentication authentication) {
-        User currentUser = helperValidateUser.getCurrentUser(authentication);
-        Post existingPost = findById(id);
-        helperComponentPostDelete.helperValidatePostDeletion(existingPost, currentUser);
+    public ResponseEntity<Void> processDeletePost(String id, Authentication authentication) {
+        User currentUser = getCurrentUser(authentication);
+        Post existingPost = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(id));
+        postDeleteComponent.validatePostDeleteComponent(existingPost, currentUser);
 
-        for (Category category : existingPost.getCategories()) {
-            helperComponentCreatePost.decrementCategoryPostCount(category.getId(), id);
-        }
+        existingPost.getCategories().forEach(category -> postDeleteComponent.decrementCategoryPostCountComponent(category.getId(), id));
+        existingPost.getTags().forEach(tag -> postDeleteComponent.decrementTagPostCountComponent(tag.getId(), id));
 
         postRepository.deleteById(id);
-        helperComponentPostDelete.helperDecrementPostCount(currentUser);
+        postDeleteComponent.decrementPostCountComponent(currentUser);
+        return ResponseEntity.noContent().build();
+    }
+
+    private void validateCurrentUser(User currentUser) {
+        if (currentUser == null) {
+            throw new NotAuthenticatedException();
+        }
+    }
+
+    private User getCurrentUser(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new NotAuthenticatedException();
+        }
+        return (User) authentication.getPrincipal();
     }
 }
