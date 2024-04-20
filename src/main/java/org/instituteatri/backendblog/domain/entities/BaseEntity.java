8013fda0fdf5b247c3a12f8587464d6d95d43b15
Rafael Silva <ainbot6@gmail.com>
2024-04-20 17:27:10 -0300

    feat: add a no-arg constructor to BaseEntity to address serialization warning (#21)

--- src/main/java/org/instituteatri/backendblog/domain/entities/BaseEntity.java
+++ src/main/java/org/instituteatri/backendblog/domain/entities/BaseEntity.java
@@ -2,11 +2,15 @@ package org.instituteatri.backendblog.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 @Getter
 @Setter
-public class BaseEntity {
+@NoArgsConstructor
+public class BaseEntity implements Serializable {
 
     protected String name;
     protected String slug;
