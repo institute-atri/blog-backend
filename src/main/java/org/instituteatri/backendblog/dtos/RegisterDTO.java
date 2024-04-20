fcac8b890093edb1bcbc09d91f9827cce972f8fb
Rafael Silva <ainbot6@gmail.com>
2024-04-20 17:28:12 -0300

    refactor: refactor regex patterns in RegisterDTO for clarity and reuse (#21)

--- src/main/java/org/instituteatri/backendblog/dtos/RegisterDTO.java
+++ src/main/java/org/instituteatri/backendblog/dtos/RegisterDTO.java
@@ -15,7 +15,7 @@ public record RegisterDTO(
 
         @NotBlank(message = "Phone number is required.")
         @Size(max = 11, message = "Phone number must be less than 11 characters.")
-        @Pattern(regexp = "^[0-9]+$", message = "Phone number must contain only digits.")
+        @Pattern(regexp = "^\\d+$", message = "Phone number must contain only digits.")
         String phoneNumber,
 
         @Size(max = 100, message = "Bio must be less than 100 characters.")
@@ -25,12 +25,12 @@ public record RegisterDTO(
         @Size(min = 10, max = 30, message = "Email must be between 10 and 30 characters.")
         String email,
         @NotBlank(message = "Password is required.")
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+).{10,30}$",
+        @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+).{10,30}$",
                 message = "Password must be strong and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
         @Size(min = 10, max = 30, message = "Password must be between 10 and 30 characters.")
         String password,
         @NotBlank(message = "Confirm password is required.")
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+).{10,30}$",
+        @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+).{10,30}$",
                 message = "Password must be strong and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
         @Size(min = 10, max = 30, message = "Password must be between 10 and 30 characters.")
         String confirmPassword
