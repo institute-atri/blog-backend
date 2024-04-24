c970031d40e59c845993f91d2e897d805e45498a
Rafael Silva <ainbot6@gmail.com>
2024-04-23 23:55:05 -0300

    refactor: refactor authentication and authorization methods for improved readability and understanding (#25)

--- src/main/java/org/instituteatri/backendblog/infrastructure/security/SecurityFilter.java
+++ src/main/java/org/instituteatri/backendblog/infrastructure/security/SecurityFilter.java
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.instituteatri.backendblog.infrastructure.exceptions.TokenInvalidException;
+import org.instituteatri.backendblog.repository.TokenRepository;
 import org.instituteatri.backendblog.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -22,6 +24,7 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
     final TokenService tokenService;
+    final TokenRepository tokenRepository;
     final UserRepository userRepository;
 
 
@@ -44,41 +47,69 @@ public class SecurityFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
 
-        var token = this.recoverToken(request);
+        String token = recoverTokenFromRequest(request);
+
         if (token != null) {
-            try {
-                if (!tokenService.isTokenInvalid(token)) {
-                    var email = tokenService.isValidateToken(token);
-                    UserDetails userDetails = userRepository.findByEmail(email);
-
-                    if (userDetails != null) {
-                        var authentication = new UsernamePasswordAuthenticationToken(
-                                userDetails,
-                                null,
-                                userDetails.getAuthorities());
-
-                        SecurityContextHolder.getContext().setAuthentication(authentication);
-                    }
-                }
-            } catch (TokenInvalidException e) {
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                response.getWriter().write(e.getMessage());
-                return;
-            }
+            handleAuthentication(token, response);
         }
+
         filterChain.doFilter(request, response);
     }
 
-    /**
-     * This method attempts to recover the JWT token from the request's authorization header.
-     *
-     * @param request the incoming HTTP request
-     * @return the JWT token, or null if not present
-     */
-
-    public String recoverToken(HttpServletRequest request) {
+    public String recoverTokenFromRequest(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
         if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
+    }
+
+
+    /**
+     * This method attempts to validate and authenticate the token.
+     * If the token is valid, the user's authentication is set in the security context.
+     *
+     * @param token    the JWT token to be validated
+     * @param response the outgoing HTTP response
+     * @throws IOException      if an I/O error occurs
+     */
+    private void handleAuthentication(String token, HttpServletResponse response)
+            throws IOException {
+
+        try {
+            String email = tokenService.isValidateToken(token);
+            UserDetails userDetails = getUserDetailsByEmail(email);
+            boolean isTokenValid = isTokenValid(token);
+
+            if (userDetails != null && isTokenValid) {
+                setAuthenticationInSecurityContext(userDetails);
+            }
+        } catch (TokenInvalidException e) {
+            handleInvalidToken(response, e);
+        }
+    }
+
+    private UserDetails getUserDetailsByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    private boolean isTokenValid(String token) {
+        return tokenRepository.findByToken(token)
+                .map(t -> !t.isExpired() && !t.isRevoked())
+                .orElse(false);
+    }
+
+    private void setAuthenticationInSecurityContext(UserDetails userDetails) {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void handleInvalidToken(HttpServletResponse response, TokenInvalidException e)
+            throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(e.getMessage());
     }
 }
