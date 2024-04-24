c90fea81b8b57fffe31fc12dfc1efc59511835f2
Rafael Silva <ainbot6@gmail.com>
2024-04-23 23:47:56 -0300

    adds TokenRepository with custom queries (#25)

---
+++ src/main/java/org/instituteatri/backendblog/repository/TokenRepository.java
@@ -0,0 +1,18 @@
+package org.instituteatri.backendblog.repository;
+
+import org.instituteatri.backendblog.domain.token.Token;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends MongoRepository<Token, String> {
+
+    @Query("{'user.id': ?0, $or: [{'expired': false}, {'revoked': false}]}")
+    List<Token> findAllValidTokenByUser(String id);
+    List<Token> findAllByUserId(String userId);
+    Optional<Token> findByToken(String token);
+}
