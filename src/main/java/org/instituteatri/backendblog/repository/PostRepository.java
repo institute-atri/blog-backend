ba92ea94bb42591089636c767a3ae74085b3a96f
Rafael Silva <ainbot6@gmail.com>
2024-04-24 21:37:50 -0300

    feat: add a new method (#20)

--- src/main/java/org/instituteatri/backendblog/repository/PostRepository.java
+++ src/main/java/org/instituteatri/backendblog/repository/PostRepository.java
@@ -4,6 +4,10 @@ import org.instituteatri.backendblog.domain.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findPostsById(String id);
 }
