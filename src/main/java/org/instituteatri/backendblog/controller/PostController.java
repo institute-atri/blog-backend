304d674a574102214f7cbf899829708bf6d4543e
Rafael Silva <ainbot6@gmail.com>
2024-05-10 16:25:52 -0300

    refactor: with the change in the names of the dtos and the new packages, the controller package has undergone these changes (#37)

--- src/main/java/org/instituteatri/backendblog/controller/PostController.java
+++ src/main/java/org/instituteatri/backendblog/controller/PostController.java
@@ -1,4 +1,4 @@
-package org.instituteatri.backendblog.controllers;
+package org.instituteatri.backendblog.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
