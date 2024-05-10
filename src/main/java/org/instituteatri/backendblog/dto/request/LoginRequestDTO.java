f0e604e1dce4be4334d89a4e2380f8b493921e33
Rafael Silva <ainbot6@gmail.com>
2024-05-10 18:55:12 -0300

    refactor: change the name of the dto to LoginRequestDTO and for the request package (#51)

--- src/main/java/org/instituteatri/backendblog/dto/request/LoginRequestDTO.java
+++ src/main/java/org/instituteatri/backendblog/dto/request/LoginRequestDTO.java
@@ -1,4 +1,4 @@
-package org.instituteatri.backendblog.dto.response;
+package org.instituteatri.backendblog.dto.request;
 
-public record LoginResponseDTO(String email, String password) {
+public record LoginRequestDTO(String email, String password) {
 }
