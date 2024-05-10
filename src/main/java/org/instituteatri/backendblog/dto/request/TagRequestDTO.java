3b18d55860db97d990bcaf2fd9c2d922b162dd49
Rafael Silva <ainbot6@gmail.com>
2024-05-10 18:35:23 -0300

    refactor: change from record to class, to be able to use the model mapper (#49)

--- src/main/java/org/instituteatri/backendblog/dto/request/TagRequestDTO.java
+++ src/main/java/org/instituteatri/backendblog/dto/request/TagRequestDTO.java
@@ -1,20 +1,19 @@
 package org.instituteatri.backendblog.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-public record TagRequestDTO(
-
-        String id,
-        @NotBlank(message = "Name is required.")
-        @Size(max = 10, message = "Name cannot be longer than 10 characters.")
-        String name,
-        @NotBlank(message = "Slug is required.")
-        @Size(max = 50, message = "Slug cannot be longer than 50 characters.")
-        String slug,
-        @JsonIgnore
-        List<PostRequestDTO> postRequestDTOS) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagRequestDTO {
+    @NotBlank(message = "Name is required.")
+    @Size(max = 10, message = "Name cannot be longer than 10 characters.")
+    private String name;
+    @NotBlank(message = "Slug is required.")
+    @Size(max = 50, message = "Slug cannot be longer than 50 characters.")
+    private String slug;
 }
