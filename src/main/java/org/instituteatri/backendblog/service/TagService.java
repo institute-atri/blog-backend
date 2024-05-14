8695746a3952207d9a8d6fe5c27e8df1bec06b95
Rafael Silva <ainbot6@gmail.com>
2024-05-13 22:09:16 -0300

    refactor: changes to the processCreateTag , processFindAllTags, findPostsByTagId and existingTagByIdOrThrow methods, nothing too out of the ordinary, just improvements according to the unit tests (#38)

--- src/main/java/org/instituteatri/backendblog/service/TagService.java
+++ src/main/java/org/instituteatri/backendblog/service/TagService.java
@@ -4,15 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.instituteatri.backendblog.domain.entities.Post;
 import org.instituteatri.backendblog.domain.entities.Tag;
 import org.instituteatri.backendblog.dto.request.TagRequestDTO;
+import org.instituteatri.backendblog.dto.request.TagUpdateRequestDTO;
 import org.instituteatri.backendblog.dto.response.PostResponseDTO;
 import org.instituteatri.backendblog.dto.response.TagResponseDTO;
+import org.instituteatri.backendblog.infrastructure.exceptions.CustomExceptionEntities;
 import org.instituteatri.backendblog.infrastructure.exceptions.TagNotFoundException;
 import org.instituteatri.backendblog.repository.PostRepository;
 import org.instituteatri.backendblog.repository.TagRepository;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -23,41 +27,59 @@ import java.util.function.Consumer;
 @RequiredArgsConstructor
 public class TagService {
 
-
+    private static final Logger log = LoggerFactory.getLogger(TagService.class);
     private final TagRepository tagRepository;
     private final PostRepository postRepository;
     private final ModelMapper modelMapper;
 
     public ResponseEntity<List<TagResponseDTO>> processFindAllTags() {
-
         List<Tag> tags = tagRepository.findAll();
-        List<TagResponseDTO> result = new ArrayList<>();
+        if (tags.isEmpty()) {
+            throw new TagNotFoundException("No tags found");
+        }
 
+        List<TagResponseDTO> result = new ArrayList<>();
         tags.forEach(x -> result.add(modelMapper.map(x, TagResponseDTO.class)));
         return ResponseEntity.ok(result);
     }
 
-    public TagResponseDTO findById(String id) {
-        return tagRepository.findById(id).map(tag -> modelMapper.map(tag, TagResponseDTO.class)).orElseThrow(() -> new TagNotFoundException(id));
+    public ResponseEntity<TagResponseDTO> findById(String id) {
+        Tag tag = existingTagByIdOrThrow(id);
+
+        TagResponseDTO tagResponse = modelMapper.map(tag, TagResponseDTO.class);
+        return ResponseEntity.ok(tagResponse);
     }
 
-    public List<PostResponseDTO> findPostsByTagId(String id) {
-
+    public ResponseEntity<List<PostResponseDTO>> findPostsByTagId(String id) {
         Tag existingTag = existingTagByIdOrThrow(id);
 
-        return existingTag.getPosts().stream().map(post -> modelMapper.map(post, PostResponseDTO.class)).toList();
+        return ResponseEntity.ok().body(
+                existingTag
+                        .getPosts()
+                        .stream()
+                        .map(post -> modelMapper.map(post, PostResponseDTO.class))
+                        .toList());
     }
 
     public ResponseEntity<TagRequestDTO> processCreateTag(TagRequestDTO tagRequestDTO) {
-        Tag tag = new Tag(tagRequestDTO.getName(), tagRequestDTO.getSlug());
+        try {
+            Tag tag = new Tag(tagRequestDTO.getName(), tagRequestDTO.getSlug());
 
-        tag = tagRepository.save(tag);
+            tag = tagRepository.save(tag);
 
-        TagRequestDTO createdTagRequestDTO = modelMapper.map(tag, TagRequestDTO.class);
+            TagRequestDTO createdTagRequestDTO = modelMapper.map(tag, TagRequestDTO.class);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(tag.getId()).toUri();
+            String baseUri = "http://localhost:8080";
+            URI uri = UriComponentsBuilder.fromUriString(baseUri)
+                    .path("/{id}")
+                    .buildAndExpand(tag.getId())
+                    .toUri();
 
-        return ResponseEntity.created(uri).body(createdTagRequestDTO);
+            return ResponseEntity.created(uri).body(createdTagRequestDTO);
+        } catch (Exception ex) {
+            log.warn("Error creating", ex);
+            throw new CustomExceptionEntities("Error creating tag.");
+        }
     }
 
     public ResponseEntity<Void> processDeleteTag(String id) {
@@ -75,8 +97,7 @@ public class TagService {
         return ResponseEntity.noContent().build();
     }
 
-
-    public ResponseEntity<Void> processUpdateTag(String id, TagRequestDTO updatedTagRequestDTO) {
+    public ResponseEntity<Void> processUpdateTag(String id, TagUpdateRequestDTO updatedTagRequestDTO) {
 
         Tag existingTag = existingTagByIdOrThrow(id);
 
@@ -88,12 +109,12 @@ public class TagService {
     }
 
     private Tag existingTagByIdOrThrow(String id) {
-        return tagRepository.findById(id).orElseThrow(() -> new TagNotFoundException(id));
+        return tagRepository.findById(id).orElseThrow(() -> new TagNotFoundException("Could not find tag with id: " + id));
     }
 
-    private void updateTagProperties(Tag existingTag, TagRequestDTO updatedTagRequestDTO) {
-        updateField(existingTag::setName, existingTag.getName(), updatedTagRequestDTO.getName());
-        updateField(existingTag::setSlug, existingTag.getSlug(), updatedTagRequestDTO.getSlug());
+    private void updateTagProperties(Tag existingTag, TagUpdateRequestDTO updatedTagRequestDTO) {
+        updateField(existingTag::setName, existingTag.getName(), updatedTagRequestDTO.name());
+        updateField(existingTag::setSlug, existingTag.getSlug(), updatedTagRequestDTO.slug());
     }
 
     private <T> void updateField(Consumer<T> setter, T currentValue, T newValue) {
