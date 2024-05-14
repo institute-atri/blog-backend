d8e626747f020a2342465f9777c8f9d836ef1369
Rafael Silva <ainbot6@gmail.com>
2024-05-13 22:06:52 -0300

    refactor: change the TagNotFoundException to receive messages directly in the method and add a condition to validate the empty user list (#38)

--- src/main/java/org/instituteatri/backendblog/infrastructure/exceptions/TagNotFoundException.java
+++ src/main/java/org/instituteatri/backendblog/infrastructure/exceptions/TagNotFoundException.java
@@ -1,7 +1,7 @@
 package org.instituteatri.backendblog.infrastructure.exceptions;
 
 public class TagNotFoundException extends RuntimeException {
-    public TagNotFoundException(String id) {
-        super("Could not find tag with id:" + id);
+    public TagNotFoundException(String message) {
+        super(message);
     }
 }
