6297cf6f5480ec5d21886c8ef2d7f682ca8e4cdf
Rafael Silva <ainbot6@gmail.com>
2024-05-13 23:28:20 -0300

    refactor: change CategoryNotFoundException to receive messages directly in the method and add a condition to validate the empty user list (#38)

--- src/main/java/org/instituteatri/backendblog/infrastructure/exceptions/CategoryNotFoundException.java
+++ src/main/java/org/instituteatri/backendblog/infrastructure/exceptions/CategoryNotFoundException.java
@@ -1,7 +1,7 @@
 package org.instituteatri.backendblog.infrastructure.exceptions;
 
 public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException(String id) {
-        super("Could not find category with id:" + id);
+    public CategoryNotFoundException(String message) {
+        super(message);
     }
 }
