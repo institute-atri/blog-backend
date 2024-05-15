d5ba316796419eeb97a31df8e20fa847a733b721
Rafael Silva <ainbot6@gmail.com>
2024-05-15 01:02:30 -0300

    refactor: remove mock to only simulate the tests with the database (#40)

--- src/test/java/org/instituteatri/backendblog/repository/UserRepositoryTest.java
+++ src/test/java/org/instituteatri/backendblog/repository/UserRepositoryTest.java
@@ -1,60 +1,54 @@
 package org.instituteatri.backendblog.repository;
 
+import org.instituteatri.backendblog.domain.entities.User;
+import org.instituteatri.backendblog.domain.entities.UserRole;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
-
-import static com.mongodb.assertions.Assertions.assertNull;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @DataMongoTest
 class UserRepositoryTest {
 
-    @MockBean
-    private UserRepository mockUserRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     @DisplayName("Should retrieve user details with valid email")
     void ShouldRetrieveUserDetailsWithValidEmail() {
         // Arrange
-        Collection<GrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
-        UserDetails mockUserDetails = new User(
+        User user = new User(
+                "Name",
+                "LastName",
+                "123456789",
+                "Bio",
                 "valid@example.com",
                 "password",
-                authorities);
-        when(mockUserRepository.findByEmail("valid@example.com")).thenReturn(mockUserDetails);
+                true,
+                UserRole.USER);
+        userRepository.save(user);
 
         // Act
-        UserDetails userDetails = mockUserRepository.findByEmail("valid@example.com");
+        UserDetails userDetails = userRepository.findByEmail(user.getEmail());
 
         // Assert
         assertNotNull(userDetails);
-        assertThat(userDetails.getUsername()).isEqualTo("valid@example.com");
-        verify(mockUserRepository).findByEmail("valid@example.com");
+        assertThat(userDetails.getUsername()).isEqualTo(user.getEmail());
+
+        userRepository.delete(user);
     }
 
     @Test
     @DisplayName("Should return null for non-existent email")
     void ShouldReturnNullForNonExistentEmail() {
-        // Arrange
-        when(mockUserRepository.findByEmail("nonexistent@example.com")).thenReturn(null);
-
         // Act
-        UserDetails userDetails = mockUserRepository.findByEmail("nonexistent@example.com");
+        UserDetails userDetails = userRepository.findByEmail("nonexistent@example.com");
 
         // Assert
-        assertNull(userDetails);
-        verify(mockUserRepository).findByEmail("nonexistent@example.com");
+        assertThat(userDetails).isNull();
     }
 }
