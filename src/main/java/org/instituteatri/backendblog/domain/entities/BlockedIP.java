a6b08d2cac60ea543488518c53c799728ad70d7b
Rafael Silva <ainbot6@gmail.com>
2024-05-16 01:23:31 -0300

    refactor: refactoring of the BlockedIP class to improve object comparison and textual representation (#39)

--- src/main/java/org/instituteatri/backendblog/domain/entities/BlockedIP.java
+++ src/main/java/org/instituteatri/backendblog/domain/entities/BlockedIP.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -27,4 +28,27 @@ public class BlockedIP implements Serializable {
     private int failedAttempts;
     private String userAgent;
     private Instant lastFailedAttemptTimestamp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BlockedIP blockedIP = (BlockedIP) o;
+        return failedAttempts == blockedIP.failedAttempts &&
+                Objects.equals(id, blockedIP.id) &&
+                Objects.equals(ipAddress, blockedIP.ipAddress) &&
+                Objects.equals(userAgent, blockedIP.userAgent) &&
+                Objects.equals(lastFailedAttemptTimestamp, blockedIP.lastFailedAttemptTimestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ipAddress, failedAttempts, userAgent, lastFailedAttemptTimestamp);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("BlockedIP(id=%s, ipAddress=%s, failedAttempts=%d, userAgent=%s, lastFailedAttemptTimestamp=%s)",
+                id, ipAddress, failedAttempts, userAgent, lastFailedAttemptTimestamp);
+    }
 }
