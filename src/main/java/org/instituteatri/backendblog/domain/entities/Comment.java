a3eeb6d69af39bf6d4b7ce4c69f27f7bd608d702
Rafael Silva <ainbot6@gmail.com>
2024-05-16 00:33:09 -0300

    feat: add setCreatedAt and setUpdatedAt methods to ensure data integrity according to unit tests (#39)

--- src/main/java/org/instituteatri/backendblog/domain/entities/Comment.java
+++ src/main/java/org/instituteatri/backendblog/domain/entities/Comment.java
@@ -11,11 +11,13 @@ import org.instituteatri.backendblog.dto.response.AuthorResponseDTO;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Comment implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
 
@@ -31,6 +33,23 @@ public class Comment implements Serializable {
     public Comment(String text, LocalDateTime createdAt, User user) {
         this.text = text;
         this.createdAt = createdAt;
-        this.authorResponseDTO = new AuthorResponseDTO(user.getName(), user.getLastName());
+        this.user = user;
+        if (user != null) {
+            this.authorResponseDTO = new AuthorResponseDTO(user.getName(), user.getLastName());
+        }
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        if (this.createdAt != null) {
+            throw new UnsupportedOperationException("createdAt cannot be updated after object creation");
+        }
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        if (updatedAt.isBefore(this.createdAt)) {
+            throw new IllegalArgumentException("updatedAt cannot be before createdAt");
+        }
+        this.updatedAt = updatedAt;
     }
 }
