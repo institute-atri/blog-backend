ac41e50888145e6b5d967e6213ee9624a2e88b26
Rafael Silva <ainbot6@gmail.com>
2024-05-16 18:15:40 -0300

    refactor: remove public modifier (#39)

--- src/test/java/org/instituteatri/backendblog/domain/token/TokenTypeTest.java
+++ src/test/java/org/instituteatri/backendblog/domain/token/TokenTypeTest.java
@@ -2,18 +2,18 @@ package org.instituteatri.backendblog.domain.token;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TokenTypeTest {
 
     @Test
-    public void testTokenTypeToString() {
+    void testTokenTypeToString() {
         assertEquals("BEARER", TokenType.BEARER.toString());
         assertEquals("BASIC", TokenType.BASIC.toString());
     }
 
     @Test
-    public void testStringToTokenType() {
+    void testStringToTokenType() {
         assertEquals(TokenType.BEARER, TokenType.valueOf("BEARER"));
         assertEquals(TokenType.BASIC, TokenType.valueOf("BASIC"));
     }
