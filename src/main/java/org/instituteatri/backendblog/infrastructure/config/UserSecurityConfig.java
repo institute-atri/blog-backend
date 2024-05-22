56c68a59fbb67d228e79884d5bbf0ed8f47d8f5f
Rafael Silva <ainbot6@gmail.com>
2024-05-21 21:21:47 -0300

    refactor: changes to use the correct functions on the endpoints (#95)

--- src/main/java/org/instituteatri/backendblog/infrastructure/config/UserSecurityConfig.java
+++ src/main/java/org/instituteatri/backendblog/infrastructure/config/UserSecurityConfig.java
@@ -8,18 +8,20 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 
 
 @Configuration
-class UserSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+public class UserSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private static final String ADMIN_ROLE = "ADMIN";
+    private static final String USER_ROLE = "USER";
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.GET, "/v1/users").hasRole(ADMIN_ROLE)
                 .requestMatchers(HttpMethod.GET, "/v1/users/find/{id}").hasRole(ADMIN_ROLE)
                 .requestMatchers(HttpMethod.DELETE, "/v1/users/delete/{id}").hasRole(ADMIN_ROLE)
-                .requestMatchers(HttpMethod.PUT, "/v1/users/update/{id}").permitAll()
+                .requestMatchers(HttpMethod.PUT, "/v1/users/update/{id}").hasAnyRole(ADMIN_ROLE, USER_ROLE)
                 .requestMatchers(HttpMethod.GET, "/v1/users/posts/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/v1/users/change-password").permitAll()
+                .requestMatchers(HttpMethod.POST, "/v1/users/change-password").hasAnyRole(ADMIN_ROLE, USER_ROLE)
         );
     }
 }
